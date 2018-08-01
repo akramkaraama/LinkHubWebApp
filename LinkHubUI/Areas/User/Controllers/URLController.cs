27467e193d07fa09cc13e1ba814c0f9148e38922
Mohamed Farah <mohamed.farah@icstars-columbus.org>
2018-08-01 01:28:26 -0400

    LinkHub MVC Web App First Commit...

---
+++ LinkHubUI/Areas/User/Controllers/URLController.cs
@@ -0,0 +1,42 @@
+﻿using BLL;
+using BOL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LinkHubUI.Areas.User.Controllers
+{
+    public class URLController : Controller
+    {
+        private UrlBs objBs;
+        private CategoryBs objCatBs;
+        private UserBs objUserBs;
+
+        public URLController()
+        {
+            objBs = new UrlBs();
+            objCatBs = new CategoryBs();
+            objUserBs = new UserBs();
+        }
+
+        // GET: User/URL
+        public ActionResult Index()
+        {
+            ViewBag.CategoryId = new SelectList(objCatBs.GetAll().ToList(), "CategoryId", "CategoryName");
+            ViewBag.UserId = new SelectList(objUserBs.GetAll().ToList(), "UserId", "UserEmail");
+            //LinkHubDbEntities db = new LinkHubDbEntities();
+            //ViewBag.CategoryId = new SelectList(db.tbl_Category, "CategoryId", "CategoryName");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(tbl_Url objUrl)
+        {
+            //LinkHubDbEntities db = new LinkHubDbEntities();
+            //ViewBag.CategoryId = new SelectList(db.tbl_Category, "CategoryId", "CategoryName");
+            return View();
+        }
+    }
+}
